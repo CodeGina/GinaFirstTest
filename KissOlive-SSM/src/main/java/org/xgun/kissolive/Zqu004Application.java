9bcae6280fee25a7cf8e05bfbf4ac64eb5b765e4
CodeGina <44261712+CodeGina@users.noreply.github.com>
2018-12-06 23:10:56 +0800

    2018-12-062310

--- KissOlive-SSM/src/main/java/org/xgun/kissolive/Zqu004Application.java
+++ KissOlive-SSM/src/main/java/org/xgun/kissolive/Zqu004Application.java
@@ -1,5 +1,6 @@
 package org.xgun.kissolive;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
